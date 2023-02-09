be276f3d7a05db9dd0ad2be0b139be481783192d
Talym <121556351+TopSoilPro@users.noreply.github.com.com>
2023-02-09 18:26:06 +1100

    Add random type selection for computer player
    Added the getComputerChoice function with associated constants to
    randomly select a type for the computer to play and return as associated
    string

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    // Constants associating the values of random with the string to return
+    const ROCK_VALUE = 0;
+    const PAPER_VALUE = 1;
+    const SCISSORS_VALUE = 2;
+    // Constants containing the strings to return
+    const ROCK = "Rock";
+    const PAPER = "Paper";
+    const SCISSORS = "Scissors";
+    // The random number value for selecting which type to play
+    let random = Math.floor(Math.random() * 3);
+}
